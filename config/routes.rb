a11566d1b6ef456823c9659de445486bda10f069
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-07 14:51:46 -0600

    user can login and out

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get '/auth/github', as: :github_login
   get '/auth/github/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
